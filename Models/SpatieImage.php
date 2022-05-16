9156d66e0f99c57569c1ca3ed1dc756dc68e7841
Malebestia <malebestia@yahoo.it>
2022-05-16 22:37:26 +0200

    .

--- Models/SpatieImage.php
+++ Models/SpatieImage.php
@@ -8,47 +8,48 @@ namespace Modules\Media\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Lang\Models\Traits\LinkedTrait;
 use Modules\Xot\Traits\Updater;
-use Spatie\MediaLibrary\MediaCollections\Models\Media as BaseMedia;
-use Spatie\Tags\HasTags; // spatie tags
+use Spatie\MediaLibrary\MediaCollections\Models\Media as BaseMedia; // spatie tags
 use Spatie\ModelStatus\HasStatuses;
+use Spatie\Tags\HasTags;
 
 /**
  * Undocumented class.
  */
 class SpatieImage extends BaseMedia {
     use Updater;
-/**
- * Indicates whether attributes are snake cased on arrays.
- *
- * @see  https://laravel-news.com/6-eloquent-secrets
-* 
- * @var bool
- */
-// public static $snakeAttributes = true;
-
-protected $perPage = 30;
 
     // use Searchable;
     // use Cachable;
     use HasFactory;
     use HasTags; // spatie tags
     use HasStatuses; //spatie status
+    use HasDomains;
     use LinkedTrait;
+    /**
+     * Indicates whether attributes are snake cased on arrays.
+     *
+     * @see  https://laravel-news.com/6-eloquent-secrets
+     *
+     * @var bool
+     */
+    // public static $snakeAttributes = true;
+
+    protected $perPage = 30;
 
     protected $fillable = [
         'id', 'model_type', 'model_id', 'uuid', 'collection_name', 'name',
-        'file_name', 'mime_type', 'disk', 'conversions_disk', 'size', 
+        'file_name', 'mime_type', 'disk', 'conversions_disk', 'size',
         'manipulations',
         'custom_properties', 'generated_conversions', 'responsive_images',
         'order_column', 'user_id',
         'time_from', 'time_to',
         'created_at', 'updated_at', 'created_by', 'updated_by',
-        'title','subtitle','guid',
+        'title', 'subtitle', 'guid',
     ];
 
     protected $appends = [
         'original_url', 'preview_url',
-        'title','subtitle',
+        'title', 'subtitle',
     ];
 
     //protected $with = ['tags:id,name'];
@@ -57,10 +58,7 @@ protected $perPage = 30;
     protected $table = 'spatie_images';
 
     /**
-     * Undocumented function
-     *
-     * @param string|null $value
-     * @return string|null
+     * Undocumented function.
      */
     public function getVideoUrlAttribute(?string $value): ?string {
         return url('/streamsnip/'.$this->id);
