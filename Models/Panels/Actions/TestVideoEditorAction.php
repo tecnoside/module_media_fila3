98f8c1f10afd53422d1cecefd0445ddd2b2581ec
marco76tv <marco.sottana@gmail.com>
2022-05-24 11:47:16 +0200

    .

--- Models/Panels/Actions/TestVideoEditorAction.php
+++ Models/Panels/Actions/TestVideoEditorAction.php
@@ -6,17 +6,17 @@ declare(strict_types=1);
 
 namespace Modules\Media\Models\Panels\Actions;
 
-//-------- services --------
+// -------- services --------
 
 use Modules\Theme\Services\ThemeService;
 use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
 
-//-------- bases -----------
+// -------- bases -----------
 
 /**
- * Class TestAction.
+ * Class TestVideoEditorAction.
  */
-class TestVideoAction extends XotBasePanelAction {
+class TestVideoEditorAction extends XotBasePanelAction {
     public bool $onItem = true;
     public string $icon = '<i class="fas fa-vial"></i>';
 
@@ -26,14 +26,14 @@ class TestVideoAction extends XotBasePanelAction {
     public function handle() {
         $video_players = [
             'plyr_io',
-            //'plyr_io.viola',
+            // 'plyr_io.viola',
             'videojs',
             'videojs.v1',
             'videojs.vue',
             'jplayer',
-            'mediaelement', //https://github.com/mediaelement/mediaelement
-            'cloudinary_video_player', //https://github.com/cloudinary/cloudinary-video-player
-            //'afterglowplayer', //afterglowplayer.com Time to say goodbye.
+            'mediaelement', // https://github.com/mediaelement/mediaelement
+            'cloudinary_video_player', // https://github.com/cloudinary/cloudinary-video-player
+            // 'afterglowplayer', //afterglowplayer.com Time to say goodbye.
         ];
         $i = request('i', 0);
 
@@ -41,7 +41,7 @@ class TestVideoAction extends XotBasePanelAction {
 
         $view_params = [
             'view' => $view,
-            'mp4_src' => '/videos/test.mp4',  //public_html/videos/
+            'mp4_src' => '/videos/test.mp4',  // public_html/videos/
             'players' => $video_players,
             'player' => $video_players[$i],
         ];
