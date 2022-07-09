779e4c48888e927a726c9df15134d9b32172ba64
Malebestia <malebestia@yahoo.it>
2022-07-09 20:18:44 +0200

    .

--- Resources/views/modal/add_customer.blade.php
+++ Resources/views/modal/add_customer.blade.php
@@ -1,9 +1,14 @@
+<x-input.group type="text" name="title"></x-input.group>
+<br>
+<x-input.group type="text" name="subtitle"></x-input.group>
+<br>
 @if (isset($form_data['tags']))
     @foreach ($form_data['tags'] as $k => $vc)
         @php
-            dddx([$form_data['tags'], $vc['label']]);
+            //dddx([$form_data['tags'], $vc['label']]);
         @endphp
         @if (isset($vc['id']))
+            {{ $vc['label'] }}
             <x-input.group type="checkbox" name="tags.{{ $k }}.active" label="{{ $vc['label'] }}">
                 </x-input>
                 {{-- <input type="text" wire:model="form_data.tags.{{ $k }}.active" />
