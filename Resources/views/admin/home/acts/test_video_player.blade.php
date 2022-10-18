1891ebc66866a1831fadc5e32c1c0df08c772df0
marco76tv <marco.sottana@gmail.com>
2022-10-18 13:26:05 +0200

    up

--- Resources/views/admin/home/acts/test_video_player.blade.php
+++ Resources/views/admin/home/acts/test_video_player.blade.php
@@ -8,8 +8,13 @@
         @endforeach
     </x-navbar>
 
-
+    @if($player!=null)
     <x-col size="3">
-        <x-video-player :player="$player" :mp4Src="$mp4_src"></x-video-player>
+        {{--  
+        <x-video-player :player="$player" :mp4Src="$mp4_src" currentTime="0"></x-video-player>
+        --}}
+        {{  $mp4_src }}
+        <video-player :mp4Src="$mp4_src" currentTime="0"></video-player>
     </x-col>
+    @endif
 @endsection
