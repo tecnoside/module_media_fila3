8e3a7aef80a451f6a1d1a2efd033f084c552fc8c
marco76tv <marco.sottana@gmail.com>
2023-01-20 10:47:46 +0100

    up

---
+++ Models/Media.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Media\Models;
+
+use Spatie\MediaLibrary\MediaCollections\Models\Media as SpatieMedia;
+
+class Media extends SpatieMedia {
+}
