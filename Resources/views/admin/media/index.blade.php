a56ec68610110b1334143a0b82241cf400313f2f
marco76tv <marco.sottana@gmail.com>
2023-01-26 17:41:01 +0100

    up

--- Resources/views/admin/media/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('adm_theme::layouts.app')
-@section('content')
-    <x-media.index
-    name="images"
-    :model="$_panel->getParent()->row"
-    collection="images" 
-    />
-@endsection
-{{ Theme::add('media::css/media.css') }}
--- docs/cache/115e16089eec4e7082f7bc5eaffed023035639e4.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?><h1 id="panel-actions">Panel Actions</h1>
-
-<p>In Laravel, panel actions are functions or methods that can be used to perform specific tasks within the Laravel admin panel. These actions are usually associated with a specific model or resource, and can be used to perform operations such as creating, updating, deleting or viewing the model or resource.</p>
-
-<p>Panel actions are typically used in the Laravel admin panel to provide users with quick and easy access to common tasks related to a particular model or resource. For example, a panel action might be used to quickly create a new user account, edit an existing user's details, or delete a user account that is no longer needed. Panel actions are typically implemented as buttons or links within the admin panel, and can be customized to fit the specific needs of a project.</p>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('_layouts.documentation', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
