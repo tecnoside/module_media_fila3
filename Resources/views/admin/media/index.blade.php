8c63f9c0b56fbd29e6fa2c893ec3bf0a2ef746a5
samueleorfei <fufoed89@gmail.com>
2023-02-02 16:32:57 +0100

    up

--- Resources/views/admin/media/index.blade.php
+++ Resources/views/admin/media/index.blade.php
@@ -1,9 +1,9 @@
 @extends('adm_theme::layouts.app')
 @section('content')
-    <x-media.index
-    name="images"
-    :model="$_panel->getParent()->row"
-    collection="images" 
-    />
+    <br>
+    <br>
+    <br>
+    <h1>Prova</h1>
+    <x-media.index name="images" :model="$_panel->getParent()->row" collection="images" />
 @endsection
 {{ Theme::add('media::css/media.css') }}
