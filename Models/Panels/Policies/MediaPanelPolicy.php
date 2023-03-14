5880e33faba0be0256be73fbd6fb1e5d6d8a246f
marco76tv <marco76tv@users.noreply.github.com>
2023-03-14 10:54:28 +0000

    Fix styling

--- Models/Panels/Policies/MediaPanelPolicy.php
+++ Models/Panels/Policies/MediaPanelPolicy.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Media\Models\Panels\Policies;
 
+use Modules\Cms\Contracts\PanelContract;
+use Modules\Cms\Models\Panels\Policies\XotBasePanelPermissionPolicy;
 use Modules\LU\Services\ProfileService;
 use Modules\Xot\Contracts\UserContract;
-use Modules\Cms\Contracts\PanelContract;
-use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
-use Modules\Cms\Models\Panels\Policies\XotBasePanelPermissionPolicy;
 
-class MediaPanelPolicy extends XotBasePanelPermissionPolicy
-{
+class MediaPanelPolicy extends XotBasePanelPermissionPolicy {
     /*
     public function index(?UserContract $user, PanelContract $panel): bool {
         return ProfileService::make()->get($user)->hasPermissionTo($panel->getPath().'-'.__FUNCTION__);
