ff929a0ffe42fcd7c340e817e7789dbdb1c3940a
marco76tv <marco.sottana@gmail.com>
2023-03-14 11:53:51 +0100

    up

--- Models/Panels/Policies/MediaPanelPolicy.php
+++ Models/Panels/Policies/MediaPanelPolicy.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Modules\Media\Models\Panels\Policies;
 
-use Modules\Cms\Contracts\PanelContract;
-use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
 use Modules\LU\Services\ProfileService;
 use Modules\Xot\Contracts\UserContract;
+use Modules\Cms\Contracts\PanelContract;
+use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
+use Modules\Cms\Models\Panels\Policies\XotBasePanelPermissionPolicy;
 
-class MediaPanelPolicy extends XotBasePanelPolicy {
+class MediaPanelPolicy extends XotBasePanelPermissionPolicy
+{
+    /*
     public function index(?UserContract $user, PanelContract $panel): bool {
         return ProfileService::make()->get($user)->hasPermissionTo($panel->getPath().'-'.__FUNCTION__);
     }
@@ -17,4 +20,5 @@ class MediaPanelPolicy extends XotBasePanelPolicy {
     public function show(?UserContract $user, PanelContract $panel): bool {
         return ProfileService::make()->get($user)->hasPermissionTo($panel->getPath().'-'.__FUNCTION__);
     }
+    */
 }
