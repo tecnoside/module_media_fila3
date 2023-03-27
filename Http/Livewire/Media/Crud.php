6661d4eb36c09695790476e73d061e31804946d4
marco76tv <marco76tv@users.noreply.github.com>
2023-03-27 20:11:47 +0000

    Fix styling

--- Http/Livewire/Media/Crud.php
+++ Http/Livewire/Media/Crud.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Modules\Media\Http\Livewire\Media;
 
-use Livewire\Component;
-use Illuminate\Support\Str;
-use Modules\Media\Traits\WithMedia;
-use Modules\Cms\Actions\GetViewAction;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Livewire\Component;
+use Modules\Cms\Actions\GetViewAction;
 use Modules\Media\Models\TemporaryUpload;
-use Modules\Media\Actions\ConvertLivewireUploadToMediaAction;
+use Modules\Media\Traits\WithMedia;
 
 class Crud extends Component {
     use WithMedia;
@@ -33,32 +32,31 @@ class Crud extends Component {
     }
 
     public function submit() {
-        //*
-        //$t1=$this->model
-            //->addFromMediaLibraryRequest($this->upload)
-            //->toMediaCollection($this->collection);
-            //->toMediaCollectionFromTemporaryUpload($this->collection,'',)
-            //$collectionName, $diskName, $this->fileName)
+        // *
+        // $t1=$this->model
+        // ->addFromMediaLibraryRequest($this->upload)
+        // ->toMediaCollection($this->collection);
+        // ->toMediaCollectionFromTemporaryUpload($this->collection,'',)
+        // $collectionName, $diskName, $this->fileName)
 
-       // session()->flash('message', 'Post successfully updated.');
-        //*/
+        // session()->flash('message', 'Post successfully updated.');
+        // */
 
-        //dddx(collect($this->upload)->pluck('order')->all());
+        // dddx(collect($this->upload)->pluck('order')->all());
         foreach ($this->upload as $attachment) {
-
             $temporaryUpload = TemporaryUpload::findByMediaUuidInCurrentSession($attachment['uuid']);
 
-            if($temporaryUpload!=null){
-                $media=$temporaryUpload->getFirstMedia();
-                //$temporaryUpload->moveMedia($this->model, $this->collection, 'media', $attachment['fileName']);
-                $media->move($this->model,$this->collection);
-            }else{
-                $media=\Modules\Media\Models\Media::findByUuid($attachment['uuid']);
+            if (null != $temporaryUpload) {
+                $media = $temporaryUpload->getFirstMedia();
+                // $temporaryUpload->moveMedia($this->model, $this->collection, 'media', $attachment['fileName']);
+                $media->move($this->model, $this->collection);
+            } else {
+                $media = \Modules\Media\Models\Media::findByUuid($attachment['uuid']);
             }
 
-            //$media->setOrder($attachment['order']);
+            // $media->setOrder($attachment['order']);
 
-            /** @var \Spatie\MediaLibrary\MediaCollections\FileAdder $fileAdder */
+            /* @var \Spatie\MediaLibrary\MediaCollections\FileAdder $fileAdder */
             /*
             $fileAdder = app(\Spatie\MediaLibrary\MediaCollections\FileAdder::class);
             $fa=$fileAdder
@@ -69,26 +67,24 @@ class Crud extends Component {
             dddx($fa);
             */
         }
-        //$order=collect($this->upload)->pluck('order')->all();
-        //\Modules\Media\Models\Media::setNewOrder($order);
+        // $order=collect($this->upload)->pluck('order')->all();
+        // \Modules\Media\Models\Media::setNewOrder($order);
 
+        // dddx($attachment);
+        /*
+        "name" => "b075a80d5f84d61ccfc0c2414063b44b.jpg"
+        "fileName" => "b075a80d5f84d61ccfc0c2414063b44b.jpg"
+        "oldUuid" => "d717a318-2abe-4ec0-ac4f-c7df4efb2e11"
+        "uuid" => "129eabc3-4b7b-4132-a072-bc748152b576"
+        "previewUrl" => "/media/7/conversions/b075a80d5f84d61ccfc0c2414063b44b-preview.jpg"
+        "order" => 1
+        "size" => 80973
+        "mime_type" => "image/jpeg"
+        "extension" => "jpg"
+        */
+        // $temporaryUpload = $temporaryUploadModelClass::findByMediaUuidInCurrentSession($uuid)
 
-
-            //dddx($attachment);
-            /*
-            "name" => "b075a80d5f84d61ccfc0c2414063b44b.jpg"
-            "fileName" => "b075a80d5f84d61ccfc0c2414063b44b.jpg"
-            "oldUuid" => "d717a318-2abe-4ec0-ac4f-c7df4efb2e11"
-            "uuid" => "129eabc3-4b7b-4132-a072-bc748152b576"
-            "previewUrl" => "/media/7/conversions/b075a80d5f84d61ccfc0c2414063b44b-preview.jpg"
-            "order" => 1
-            "size" => 80973
-            "mime_type" => "image/jpeg"
-            "extension" => "jpg"
-            */
-            //$temporaryUpload = $temporaryUploadModelClass::findByMediaUuidInCurrentSession($uuid)
-
-        //}
+        // }
     }
 
     public function submitVecio() {
