31c855686684fc58faaf6bfa8110a6cc389626ab
marco76tv <marco.sottana@gmail.com>
2023-04-12 17:06:06 +0200

    up

--- Resources/views/components/media/upload/v1.blade.php
+++ Resources/views/components/media/upload/v1.blade.php
@@ -1,9 +1,4 @@
 @include('media::components.partials.media-library-once')
-<<<<<<< HEAD <livewire:media.index :key="'media-library-component' . $name" :media="$media" :name="$name" :rules="$rules"
-    :multiple="$multiple" :maxItems="$determineMaxItems()" :view="$componentView ?? null" :sortable="false" :listView="$determineListViewName()" :itemView="$determineItemViewName()"
-    :propertiesView="$propertiesView ?? null" :fieldsView="$determineFieldsViewName()" :editableName="$editableName" />
-=======
-<livewire:media.index :key="'media-library-component' . $name" :media="$media" :name="$name" :rules="$rules" :multiple="$multiple"
-    :maxItems="$determineMaxItems()" :view="$componentView ?? null" :sortable="false" :listView="$determineListViewName()" :itemView="$determineItemViewName()" :propertiesView="$propertiesView ?? null"
-    :fieldsView="$determineFieldsViewName()" :editableName="$editableName" />
->>>>>>> a573407 (up)
+:multiple="$multiple" :maxItems="$determineMaxItems()" :view="$componentView ?? null" :sortable="false"
+:listView="$determineListViewName()" :itemView="$determineItemViewName()"
+:propertiesView="$propertiesView ?? null" :fieldsView="$determineFieldsViewName()" :editableName="$editableName" />
