33460feb54d161f622555720ff7dfd70b8e6d91a
marco76tv <marco.sottana@gmail.com>
2023-05-03 12:11:53 +0200

    up

--- Http/Livewire/Media/Index.php
+++ Http/Livewire/Media/Index.php
@@ -10,7 +10,6 @@ use Illuminate\Support\ViewErrorBag;
 use Livewire\Component;
 use Modules\Media\Dto\ViewMediaItem;
 
-<<<<<<< Updated upstream
 class Index extends Component
 {
     public $name;
@@ -27,23 +26,6 @@ class Index extends Component
     public $fieldsView;
     public $listErrorMessage = null;
     protected $validationErrors = null;
-=======
-class Index extends Component {
-    public string $name;
-    public bool $multiple;
-    public bool $sortable;
-    public bool $editableName;
-    public string $rules;
-    public ?int $maxItems = null;
-    public array $media;
-    public ?string $view;
-    public ?string $listView; // ?????????
-    public ?string $itemView; // ?????????
-    public ?string $propertiesView; // ???????
-    public ?string $fieldsView; // ??????????
-    public ?string $listErrorMessage = null;
-    protected MessageBag $validationErrors;
->>>>>>> Stashed changes
 
     public function mount(
         string $name,
@@ -78,12 +60,8 @@ class Index extends Component {
         $this->listErrorMessage = $this->determineListErrorMessage();
     }
 
-<<<<<<< Updated upstream
     protected function getListeners()
     {
-=======
-    protected function getListeners(): array {
->>>>>>> Stashed changes
         return [
             "{$this->name}:fileAdded" => 'onFileAdded',
             "{$this->name}:uploadError" => 'onUploadError',
@@ -93,7 +71,8 @@ class Index extends Component {
         ];
     }
 
-    public function onFileAdded(array $newMediaItem): void {
+    public function onFileAdded(array $newMediaItem): void
+    {
         if (! $this->allowsUpload($newMediaItem)) {
             return;
         }
@@ -112,7 +91,8 @@ class Index extends Component {
         $this->emit("{$this->name}:mediaChanged", $this->name, $this->media);
     }
 
-    public function remove(string $uuid): void {
+    public function remove(string $uuid): void
+    {
         $this->media = collect($this->media)
             ->reject(fn (array $mediaItem) => $mediaItem['uuid'] === $uuid)
             ->toArray();
@@ -120,7 +100,8 @@ class Index extends Component {
         $this->emit("{$this->name}:mediaChanged", $this->name, $this->media);
     }
 
-    public function allowsUpload(array $mediaItem): bool {
+    public function allowsUpload(array $mediaItem): bool
+    {
         if ($this->isReplacing($mediaItem)) {
             return true;
         }
@@ -128,7 +109,8 @@ class Index extends Component {
         return $this->allowsUploads();
     }
 
-    public function allowsUploads(): bool {
+    public function allowsUploads(): bool
+    {
         if (is_null($this->maxItems)) {
             return true;
         }
@@ -136,7 +118,8 @@ class Index extends Component {
         return (is_countable($this->media) ? count($this->media) : 0) < $this->maxItems;
     }
 
-    public function isReplacing(array $newMediaItem): bool {
+    public function isReplacing(array $newMediaItem): bool
+    {
         if (! $newMediaItem['oldUuid']) {
             return false;
         }
@@ -145,12 +128,8 @@ class Index extends Component {
             ->contains(fn (array $existingMediaItem): bool => $existingMediaItem['uuid'] === $newMediaItem['oldUuid']);
     }
 
-<<<<<<< Updated upstream
     public function hideError(string $uuid)
     {
-=======
-    public function hideError(string $uuid): void {
->>>>>>> Stashed changes
         if (! isset($this->media[$uuid])) {
             return;
         }
@@ -158,7 +137,8 @@ class Index extends Component {
         $this->media[$uuid]['hideError'] = true;
     }
 
-    public function determineListErrorMessage(MessageBag $viewErrorBag = null): ?string {
+    public function determineListErrorMessage(MessageBag $viewErrorBag = null): ?string
+    {
         if ($viewErrorBag) {
             return $viewErrorBag->first($this->name);
         }
@@ -172,7 +152,6 @@ class Index extends Component {
         return $errors->first($this->name);
     }
 
-<<<<<<< Updated upstream
     public function clearListErrorMessage()
     {
         $this->listErrorMessage = null;
@@ -180,13 +159,6 @@ class Index extends Component {
 
     public function onUploadError(string $uuid, string $uploadError)
     {
-=======
-    public function clearListErrorMessage(): void {
-        $this->listErrorMessage = null;
-    }
-
-    public function onUploadError(string $uuid, string $uploadError): void {
->>>>>>> Stashed changes
         if (! isset($this->media[$uuid])) {
             return;
         }
@@ -194,7 +166,6 @@ class Index extends Component {
         $this->media[$uuid]['uploadError'] = $uploadError;
     }
 
-<<<<<<< Updated upstream
     public function onShowListErrorMessage(string $message)
     {
         $this->listErrorMessage = $message;
@@ -202,13 +173,6 @@ class Index extends Component {
 
     public function onMediaComponentValidationErrors(string $componentName, array $validationErrors)
     {
-=======
-    public function onShowListErrorMessage(string $message): void {
-        $this->listErrorMessage = $message;
-    }
-
-    public function onMediaComponentValidationErrors(string $componentName, array $validationErrors): void {
->>>>>>> Stashed changes
         if ($componentName !== $this->name) {
             return;
         }
@@ -224,7 +188,8 @@ class Index extends Component {
         $this->validationErrors = $messageBag;
     }
 
-    public function onClearMedia(string $componentName): void {
+    public function onClearMedia(string $componentName): void
+    {
         if ($componentName !== $this->name) {
             return;
         }
@@ -235,51 +200,33 @@ class Index extends Component {
         $this->emit("{$this->name}:mediaChanged", $this->name, $this->media);
     }
 
-<<<<<<< Updated upstream
     public function setMediaProperty(string $uuid, string $attributeName, $value)
     {
-=======
-    public function setMediaProperty(string $uuid, string $attributeName, string $value): void {
->>>>>>> Stashed changes
         $this->media[$uuid][$attributeName] = $value;
 
         $this->emit("{$this->name}:mediaChanged", $this->name, $this->media);
     }
 
-<<<<<<< Updated upstream
     public function setCustomProperty(string $uuid, string $customPropertyName, $value)
     {
-=======
-    public function setCustomProperty(string $uuid, string $customPropertyName, string $value): void {
->>>>>>> Stashed changes
         Arr::set($this->media, "{$uuid}.custom_properties.{$customPropertyName}", $value);
 
         $this->emit("{$this->name}:mediaChanged", $this->name, $this->media);
     }
 
-<<<<<<< Updated upstream
     public function setNewOrder(array $newOrder)
     {
-=======
-    public function setNewOrder(array $newOrder): void {
->>>>>>> Stashed changes
         foreach ($newOrder as $newOrderItem) {
-            Arr::set($this->media, "{$newOrderItem['uuid']}.order", $newOrderItem['order']);
+            $this->media = collect($this->media)
+                ->sortBy('order')
+                ->toArray();
+
+            $this->emit("{$this->name}:mediaChanged", $this->name, $this->media);
         }
-
-        $this->media = collect($this->media)
-            ->sortBy('order')
-            ->toArray();
-
-        $this->emit("{$this->name}:mediaChanged", $this->name, $this->media);
     }
 
-<<<<<<< Updated upstream
     public function render()
     {
-=======
-    public function render(): Renderable {
->>>>>>> Stashed changes
         return view($this->view, [
             'errors' => $this->validationErrors,
             'sortedMedia' => collect($this->media)
