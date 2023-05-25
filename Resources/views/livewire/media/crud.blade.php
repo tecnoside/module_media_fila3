9ed176e5c66e3aae83a9b8da607889ba59ab1bad
samueleorfei <fufoed89@gmail.com>
2023-05-25 12:14:47 +0200

    up

--- Resources/views/livewire/media/crud.blade.php
+++ Resources/views/livewire/media/crud.blade.php
@@ -1,6 +1,5 @@
 <form method="POST" wire:submit.prevent="submit">
     <x-flash-message />
-    aaaa bbb
     <x-media.index :name="$name" :model="$model" :collection="$collection" rules="max:1000000" />
     <x-button type="submit">Salva</x-button>
 </form>
