1a6f38c636fe292d679220491b93d5c7f89dfd92
marco76tv <marco.sottana@gmail.com>
2023-06-12 12:42:25 +0200

    up

--- View/Components/Media/Upload.php
+++ View/Components/Media/Upload.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Modules\Media\View\Components\Media;
 
+use Illuminate\Contracts\Support\Renderable;
 use Illuminate\View\Component;
+use Modules\Cms\Actions\GetViewAction;
 
 class Upload extends Component
 {
     public array $media;
+    public string $tpl = 'v1';
 
     public ?string $propertiesView = null;
 
@@ -32,9 +35,25 @@ class Upload extends Component
         $this->propertiesView = $propertiesView ?? 'media::livewire.partials.attachment.properties';
     }
 
-    public function render()
+   /* public function render()
     {
         return view('media::components.media-library-attachment');
+    }*/
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): Renderable
+    {
+        /**
+         * @phpstan-var view-string
+         */
+        $view = app(GetViewAction::class)->execute($this->tpl);
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view($view, $view_params);
     }
 
     public function determineListViewName(): string
