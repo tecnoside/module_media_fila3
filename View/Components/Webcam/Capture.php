b70ec4397ee413ce0d5958df83330fe007d7abdd
marco76tv <marco.sottana@gmail.com>
2023-06-12 17:32:19 +0200

    up

---
+++ View/Components/Webcam/Capture.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Media\View\Components\Webcam;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\View\Component;
+use Modules\Cms\Actions\GetViewAction;
+
+class Capture extends Component
+{
+    public string $tpl = 'v1';
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): Renderable
+    {
+        /**
+         * @phpstan-var view-string
+         */
+        $view = app(GetViewAction::class)->execute($this->tpl);
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view($view, $view_params);
+    }
+}
