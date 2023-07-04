509c247a0c938cc3117234fa40278bfda50b7a9e
samueleorfei <fufoed89@gmail.com>
2023-07-04 18:36:19 +0200

    up

---
+++ Database/Migrations/2021_01_01_000004_create_media_library_item_table.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Syntax error or access violation: 1118 Row size too large. The maximum row size for the used table type, not counting BLOBs, is 8126. This includes storage overhead, check the manual. You have to change some columns to TEXT or BLOBs (SQL: alter table `places` add `address` text null).
+ */
+
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+// ----- models -----
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+
+/**
+ * Class CreateVideosTable.
+ */
+class CreateMediaLibraryItemTable extends XotBaseMigration {
+    /**
+     * i don't write table name, it take from Model, model is singular of this class wit.
+     *
+     * @return void
+     */
+    public function up() {
+        // -- CREATE --
+        $this->tableCreate(
+            function (Blueprint $table) {
+                $table->increments('id');
+            }
+        );
+
+        // -- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table) {
+                if (! $this->hasColumn('order_column')) {
+                    $table->integer('order_column')->nullable();
+                }
+            }
+        );
+    }
+}
