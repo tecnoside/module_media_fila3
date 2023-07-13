9e3b419e73b21221a4ea7e4fdc4a3001443ed741
samueleorfei <fufoed89@gmail.com>
2023-07-13 12:28:15 +0200

    up

---
+++ Http/Livewire/Media/CustomProperty.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Modules\Media\Http\Livewire\Media;
+
+use Filament\Forms;
+use Livewire\Component;
+
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class CustomProperty extends Component implements Forms\Contracts\HasForms
+{
+    use Forms\Concerns\InteractsWithForms;
+
+    public string $attributeName;
+    public Media $media;
+
+    public function mount(Media $media, string $attributeName='description')
+    {
+        $this->media=$media;
+        $this->attributeName=$attributeName;
+        $value=$this->media->getCustomProperty($attributeName);
+        $this->form->fill([
+            $this->attributeName => $value,
+        ]);
+
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            Forms\Components\Textarea::make($this->attributeName)
+                ->label('')
+                //->reactive()
+                ->lazy()
+                ->afterStateUpdated(function ($state) {
+                    $this->media->setCustomProperty($this->attributeName, $state);
+                    $this->media->save();
+                }),
+            // ...
+        ];
+    }
+
+    public function render()
+    {
+        /**
+         * @phpstan-var view-string
+         */
+        //$view = app(GetViewAction::class)->execute();
+        $view='media::livewire.media.custom-property';
+
+        return view($view);
+        
+    }
+}
