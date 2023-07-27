fb42852be60aaacfed603e1de1083303d7bd793f
marco76tv <marco.sottana@gmail.com>
2023-07-27 10:22:38 +0200

    up

--- Resources/views/livewire/media/crud.blade.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" wire:submit.prevent="submit">
-    <x-flash-message />
-    <x-media.index :name="$name" :model="$model" :collection="$collection" rules="max:1000000" fields-view="media::livewire.partials.collection.fields.description" />
-    <x-button type="submit">Salva</x-button>
-</form>
