d2b48f9208ce1b49b4e6c9f9be0f93dff9376bd5
marco76tv <marco76tv@users.noreply.github.com>
2023-08-02 10:05:11 +0000

    Check & fix styling

--- Models/MediaLibraryItem.php
+++ Models/MediaLibraryItem.php
@@ -9,33 +9,35 @@ use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
 
 /**
- * Modules\Media\Models\MediaLibraryItem
+ * Modules\Media\Models\MediaLibraryItem.
  *
- * @property int $id
- * @property int|null $uploaded_by_user_id
- * @property string|null $caption
- * @property string|null $alt_text
- * @property int|null $folder_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property int|null $order_column
- * @property-read \RalphJSmit\Filament\MediaLibrary\Media\Models\MediaLibraryFolder|null $folder
- * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
- * @property-read int|null $media_count
- * @property-read \Modules\User\Models\User|null $user
+ * @property int                                                                                                                               $id
+ * @property int|null                                                                                                                          $uploaded_by_user_id
+ * @property string|null                                                                                                                       $caption
+ * @property string|null                                                                                                                       $alt_text
+ * @property int|null                                                                                                                          $folder_id
+ * @property \Illuminate\Support\Carbon|null                                                                                                   $created_at
+ * @property \Illuminate\Support\Carbon|null                                                                                                   $updated_at
+ * @property int|null                                                                                                                          $order_column
+ * @property \RalphJSmit\Filament\MediaLibrary\Media\Models\MediaLibraryFolder|null                                                            $folder
+ * @property \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property int|null                                                                                                                          $media_count
+ * @property \Modules\User\Models\User|null                                                                                                    $user
+ *
  * @method static \RalphJSmit\Filament\MediaLibrary\Database\Factories\Media\MediaLibraryFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem ordered(string $direction = 'asc')
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem query()
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem whereAltText($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem whereCaption($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem whereFolderId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem whereOrderColumn($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem whereUploadedByUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         ordered(string $direction = 'asc')
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         query()
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         whereAltText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         whereCaption($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         whereFolderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         whereOrderColumn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MediaLibraryItem                         whereUploadedByUserId($value)
+ *
  * @mixin \Eloquent
  */
 class MediaLibraryItem extends BaseMediaLibraryItem implements Sortable
