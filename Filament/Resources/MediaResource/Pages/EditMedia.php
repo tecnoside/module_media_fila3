4fabd7cdcd123ebe7f41496ab200a8ae4ca04e1e
marco76tv <marco.sottana@gmail.com>
2023-10-02 23:27:12 +0200

    up

---
+++ Filament/Resources/MediaResource/Pages/EditMedia.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Media\Filament\Resources\MediaResource\Pages;
+
+use Modules\Media\Filament\Resources\MediaResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditMedia extends EditRecord
+{
+    protected static string $resource = MediaResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
---
+++ Filament/Resources/TemporaryUploadResource/Pages/CreateTemporaryUpload.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Media\Filament\Resources\TemporaryUploadResource\Pages;
+
+use Modules\Media\Filament\Resources\TemporaryUploadResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTemporaryUpload extends CreateRecord
+{
+    protected static string $resource = TemporaryUploadResource::class;
+}
