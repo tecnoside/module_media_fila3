52064e946a22abc24edd9d7d87c299d68d5babd5
marco76tv <marco.sottana@gmail.com>
2023-10-09 16:14:08 +0200

    .

--- Database/Migrations/2021_01_01_000005_create_media_library_folders_table.php
+++ Database/Migrations/2021_01_01_000005_create_media_library_folders_table.php
@@ -12,7 +12,7 @@ use Modules\Xot\Database\Migrations\XotBaseMigration;
 /**
  * Class CreateVideosTable.
  */
-class CreateMediaLibraryItemsTable extends XotBaseMigration
+class CreateMediaLibraryFoldersTable extends XotBaseMigration
 {
     /**
      * i don't write table name, it take from Model, model is singular of this class wit.
@@ -25,17 +25,9 @@ class CreateMediaLibraryItemsTable extends XotBaseMigration
         $this->tableCreate(
             function (Blueprint $table) {
                 $table->id();
+                $table->foreignId('parent_id')->nullable();
+                $table->string('name');
 
-                // $table->foreignId('uploaded_by_user_id')
-                //    ->nullable()
-                // ->constrained('users')
-                // ->nullOnDelete()
-                // ->cascadeOnUpdate()
-
-                $table->foreignUuid('uploaded_by_user_id')->nullable();
-                $table->string('caption')->nullable();
-                $table->string('alt_text')->nullable();
-                $table->integer('order_column')->nullable();
                 $this->timestamps($table);
             }
         );
@@ -45,11 +37,6 @@ class CreateMediaLibraryItemsTable extends XotBaseMigration
             function (Blueprint $table) {
                 if (! $this->hasColumn('order_column')) {
                     $table->integer('order_column')->nullable();
-                }
-                if (! $this->hasColumn('folder_id')) {
-                    $table->foreignId('folder_id')->nullable()->after('alt_text')
-                        // ->constrained('filament_media_library_folders');
-                    ;
                 }
             }
         );
