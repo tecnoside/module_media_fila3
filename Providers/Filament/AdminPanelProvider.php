f5147bc80622f8575389c0b2aa025961cf0afe73
samueleorfei <fufoed89@gmail.com>
2023-10-09 12:10:40 +0200

    up

--- Providers/Filament/AdminPanelProvider.php
+++ Providers/Filament/AdminPanelProvider.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
+<<<<<<< HEAD
 namespace Modules\Media\Providers\Filament;
+=======
+namespace App\Providers\Filament;
+>>>>>>> 02fd505 (.)
 
 use Modules\Xot\Providers\Filament\XotBasePanelProvider;
 
