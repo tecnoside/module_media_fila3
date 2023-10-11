8f46b399e481b18b87724da492eb29191948cfdf
marco76tv <marco.sottana@gmail.com>
2023-10-11 12:08:04 +0200

    rebase

--- Actions/GetVideoFrameContentAction.php
+++ Actions/GetVideoFrameContentAction.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 
 namespace Modules\Media\Actions;
 
-use Exception;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -40,7 +39,7 @@ class GetVideoFrameContentAction
             return '';
         }
         $seconds = 3600;
-        $cache_key = Str::slug($disk_mp4 . ' ' . $file_mp4 . ' ' . $time . ' 1');
+        $cache_key = Str::slug($disk_mp4.' '.$file_mp4.' '.$time.' 1');
 
         return Cache::store('file')->remember(
             $cache_key,
@@ -52,7 +51,7 @@ class GetVideoFrameContentAction
                         ->getFrameFromSeconds($time)
                         ->export()
                         ->getFrameContents();
-                } catch (Exception) {
+                } catch (\Exception) {
                     return Storage::disk('public_html')->get('img/video_not_exists.jpg');
                 }
             }
