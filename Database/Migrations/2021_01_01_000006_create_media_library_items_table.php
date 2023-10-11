6953ee22a017ebbf364f0aa9305b8a1c4eb88860
marco76tv <marco.sottana@gmail.com>
2023-10-11 12:08:13 +0200

    rebase

--- Database/Migrations/2021_01_01_000006_create_media_library_items_table.php
+++ Database/Migrations/2021_01_01_000006_create_media_library_items_table.php
@@ -12,7 +12,15 @@ use Modules\Xot\Database\Migrations\XotBaseMigration;
 /**
  * Class CreateVideosTable.
  */
+<<<<<<< HEAD
 class CreateMediaLibraryItemsTable extends XotBaseMigration
+=======
+<<<<<<<< HEAD:Database/Migrations/2021_01_01_000005_create_media_library_folders_table.php
+class CreateMediaLibraryFoldersTable extends XotBaseMigration
+========
+class CreateMediaLibraryItemsTable extends XotBaseMigration
+>>>>>>>> 0bc0ff6 (up):Database/Migrations/2021_01_01_000005_create_media_library_items_table.php
+>>>>>>> 0f96da5 (.)
 {
     /**
      * i don't write table name, it take from Model, model is singular of this class wit.
@@ -25,6 +33,14 @@ class CreateMediaLibraryItemsTable extends XotBaseMigration
         $this->tableCreate(
             function (Blueprint $table) {
                 $table->id();
+<<<<<<< HEAD
+=======
+<<<<<<<< HEAD:Database/Migrations/2021_01_01_000005_create_media_library_folders_table.php
+                $table->foreignId('parent_id')->nullable();
+                $table->string('name');
+
+========
+>>>>>>> 0f96da5 (.)
 
                 // $table->foreignId('uploaded_by_user_id')
                 //    ->nullable()
@@ -36,6 +52,10 @@ class CreateMediaLibraryItemsTable extends XotBaseMigration
                 $table->string('caption')->nullable();
                 $table->string('alt_text')->nullable();
                 $table->integer('order_column')->nullable();
+<<<<<<< HEAD
+=======
+>>>>>>>> 0bc0ff6 (up):Database/Migrations/2021_01_01_000005_create_media_library_items_table.php
+>>>>>>> 0f96da5 (.)
                 $this->timestamps($table);
             }
         );
@@ -47,8 +67,14 @@ class CreateMediaLibraryItemsTable extends XotBaseMigration
                     $table->integer('order_column')->nullable();
                 }
                 if (! $this->hasColumn('folder_id')) {
+<<<<<<< HEAD
                     $table->foreignId('folder_id')->nullable()->after('alt_text');
                     // ->constrained('filament_media_library_folders');
+=======
+                    $table->foreignId('folder_id')->nullable()->after('alt_text')
+                        // ->constrained('filament_media_library_folders');
+                    ;
+>>>>>>> 0f96da5 (.)
                 }
             }
         );
