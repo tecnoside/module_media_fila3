1084257b0bf8f0066ece12f6d73263f94aa1486a
marco76tv <marco.sottana@gmail.com>
2023-10-11 12:08:09 +0200

    rebase

--- Database/Migrations/2021_01_01_000005_create_media_library_folders_table.php
+++ Database/Migrations/2021_01_01_000005_create_media_library_folders_table.php
@@ -12,11 +12,15 @@ use Modules\Xot\Database\Migrations\XotBaseMigration;
 /**
  * Class CreateVideosTable.
  */
+<<<<<<< HEAD
 <<<<<<<< HEAD:Database/Migrations/2021_01_01_000005_create_media_library_folders_table.php
 class CreateMediaLibraryFoldersTable extends XotBaseMigration
 ========
 class CreateMediaLibraryItemsTable extends XotBaseMigration
 >>>>>>>> 0bc0ff6 (up):Database/Migrations/2021_01_01_000005_create_media_library_items_table.php
+=======
+class CreateMediaLibraryFoldersTable extends XotBaseMigration
+>>>>>>> 6de5f29 (.)
 {
     /**
      * i don't write table name, it take from Model, model is singular of this class wit.
@@ -29,6 +33,7 @@ class CreateMediaLibraryItemsTable extends XotBaseMigration
         $this->tableCreate(
             function (Blueprint $table) {
                 $table->id();
+<<<<<<< HEAD
 <<<<<<<< HEAD:Database/Migrations/2021_01_01_000005_create_media_library_folders_table.php
                 $table->foreignId('parent_id')->nullable();
                 $table->string('name');
@@ -46,6 +51,11 @@ class CreateMediaLibraryItemsTable extends XotBaseMigration
                 $table->string('alt_text')->nullable();
                 $table->integer('order_column')->nullable();
 >>>>>>>> 0bc0ff6 (up):Database/Migrations/2021_01_01_000005_create_media_library_items_table.php
+=======
+                $table->foreignId('parent_id')->nullable();
+                $table->string('name');
+
+>>>>>>> 6de5f29 (.)
                 $this->timestamps($table);
             }
         );
