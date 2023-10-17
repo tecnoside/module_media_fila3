ed2c51e8b1b9f042e7850e6965200fcae2dfcada
marco76tv <marco76tv@users.noreply.github.com>
2023-10-17 06:51:40 +0000

    Check & fix styling

--- Rules/FileExtensionRule.php
+++ Rules/FileExtensionRule.php
@@ -7,8 +7,6 @@ namespace Modules\Media\Rules;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Http\UploadedFile;
 
-use function in_array;
-
 class FileExtensionRule implements Rule
 {
     protected array $validExtensions = [];
@@ -22,12 +20,12 @@ class FileExtensionRule implements Rule
     }
 
     /**
-     * @param  string  $attribute
-     * @param  UploadedFile  $value
+     * @param string       $attribute
+     * @param UploadedFile $value
      */
     public function passes($attribute, $value): bool
     {
-        return in_array(
+        return \in_array(
             strtolower($value->getClientOriginalExtension()),
             $this->validExtensions,
             strict: true,
