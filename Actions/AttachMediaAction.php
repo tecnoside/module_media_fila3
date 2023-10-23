27b5e70098976d8eb00c8ec42921568f2e198181
marco76tv <marco.sottana@gmail.com>
2023-10-23 11:40:13 +0200

    up

--- Actions/AttachMediaAction.php
+++ Actions/AttachMediaAction.php
@@ -1,69 +1,54 @@
 <?php
 
+/**
+ * @see https://github.com/protonemedia/laravel-ffmpeg
+ */
+
 declare(strict_types=1);
 
 namespace Modules\Media\Actions;
 
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Str;
-use Livewire\FileUploadConfiguration;
-use Livewire\TemporaryUploadedFile;
 use Modules\Media\Models\Media;
 use Modules\Media\Models\TemporaryUpload;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\QueueableAction\QueueableAction;
 
-class ConvertLivewireUploadToMediaAction
+class AttachMediaAction
 {
-    public function execute(TemporaryUploadedFile $temporaryUploadedFile): Media
+    use QueueableAction;
+
+    /**
+     * Create a new action instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        return $this->isLocalTemporaryDisk()
-            ? $this->createFromLocalLivewireUpload($temporaryUploadedFile)
-            : $this->createFromRemoteLivewireUpload($temporaryUploadedFile);
+        // Prepare the action for execution, leveraging constructor injection.
     }
 
-    protected function isLocalTemporaryDisk(): bool
+    /**
+     * Execute the action.
+     */
+    public function execute(HasMedia $hasMedia, array $attachments, string $collection = null): void
     {
-        // See \Livewire\FileUploadConfiguration::isUsingS3()
+        $order = 1;
+        foreach ($attachments as $attachment) {
+            ++$order;
+            $temporaryUpload = TemporaryUpload::findByMediaUuidInCurrentSession($attachment['uuid']);
 
-        $diskBeforeTestFake = config('livewire.temporary_file_upload.disk') ?: config('filesystems.default');
+            if ($temporaryUpload instanceof TemporaryUpload) {
+                // $media = $temporaryUpload->getFirstMedia();
+                $media = $temporaryUpload->moveMedia($hasMedia, $collection, '', $attachment['fileName']);
+                // dddx($res);
+                // $media->move($this->model, $this->collection);
+            } else {
+                $media = Media::findByUuid($attachment['uuid']);
+                // $media->update(['order_column'=>$order]);
+                // dddx(['media'=>$media,'order'=>$order]);
+            }
 
-        return 'local' === config('filesystems.disks.' . strtolower((string) $diskBeforeTestFake) . '.driver');
-    }
-
-    protected function createFromLocalLivewireUpload(TemporaryUploadedFile $temporaryUploadedFile): Media
-    {
-        $uploadedFile = new UploadedFile($temporaryUploadedFile->path(), $temporaryUploadedFile->getClientOriginalName());
-
-        /** @var class-string<TemporaryUpload> $temporaryUploadModelClass */
-        $temporaryUploadModelClass = config('media-library.temporary_upload_model');
-
-        $temporaryUpload = $temporaryUploadModelClass::createForFile(
-            $uploadedFile,
-            session()->getId(),
-            (string) Str::uuid(),
-            $temporaryUploadedFile->getClientOriginalName()
-        );
-
-        return $temporaryUpload->getFirstMedia();
-    }
-
-    protected function createFromRemoteLivewireUpload(TemporaryUploadedFile $temporaryUploadedFile): Media
-    {
-        /** @var class-string<TemporaryUpload> $temporaryUploadModelClass */
-        $temporaryUploadModelClass = config('media-library.temporary_upload_model');
-
-        $livewireDisk = config('livewire.temporary_file_upload.disk', 's3');
-
-        $livewireDirectory = FileUploadConfiguration::directory();
-        $remotePath = Str::of($livewireDirectory)->start('/')->finish('/') . $temporaryUploadedFile->getFilename();
-
-        $temporaryUpload = $temporaryUploadModelClass::createForRemoteFile(
-            $remotePath,
-            session()->getId(),
-            (string) Str::uuid(),
-            $temporaryUploadedFile->getClientOriginalName(),
-            $livewireDisk
-        );
-
-        return $temporaryUpload->getFirstMedia();
+            $media?->update(['order_column' => $order, 'custom_properties' => $attachment['custom_properties'] ?? []]);
+        }
     }
 }
