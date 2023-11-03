5d9d19b8f0270e1ad304d044a95844e67b1729ed
marco76tv <marco76tv@users.noreply.github.com>
2023-11-03 11:35:13 +0000

    Check & fix styling

--- Models/MediaLibraryFolder.php
+++ Models/MediaLibraryFolder.php
@@ -6,28 +6,25 @@ namespace Modules\Media\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Carbon;
-use Modules\User\Models\User;
-use RalphJSmit\Filament\MediaLibrary\Database\Factories\Media\MediaLibraryFactory;
 use RalphJSmit\Filament\MediaLibrary\Media\Models\MediaLibraryFolder as BaseMediaLibraryFolder;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
-use Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 /**
  * Modules\Media\Models\MediaLibraryItem.
  *
- * @property int $id
- * @property int|null $parent_id
- * @property string $name
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property string|null $user_id
- * @property string|null $updated_by
- * @property string|null $created_by
- * @property int|null $order_column
- * @property-read \Illuminate\Database\Eloquent\Collection<int, MediaLibraryFolder> $children
- * @property-read int|null $children_count
+ * @property int                                                               $id
+ * @property int|null                                                          $parent_id
+ * @property string                                                            $name
+ * @property Carbon|null                                                       $created_at
+ * @property Carbon|null                                                       $updated_at
+ * @property string|null                                                       $user_id
+ * @property string|null                                                       $updated_by
+ * @property string|null                                                       $created_by
+ * @property int|null                                                          $order_column
+ * @property \Illuminate\Database\Eloquent\Collection<int, MediaLibraryFolder> $children
+ * @property int|null                                                          $children_count
+ *
  * @method static Builder|MediaLibraryFolder newModelQuery()
  * @method static Builder|MediaLibraryFolder newQuery()
  * @method static Builder|MediaLibraryFolder ordered(string $direction = 'asc')
@@ -41,6 +38,7 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
  * @method static Builder|MediaLibraryFolder whereUpdatedAt($value)
  * @method static Builder|MediaLibraryFolder whereUpdatedBy($value)
  * @method static Builder|MediaLibraryFolder whereUserId($value)
+ *
  * @mixin \Eloquent
  */
 class MediaLibraryFolder extends BaseMediaLibraryFolder implements Sortable
--- Models/TemporaryUpload.php
+++ Models/TemporaryUpload.php
@@ -18,12 +18,13 @@ use Spatie\MediaLibrary\InteractsWithMedia;
 /**
  * Modules\Media\Models\TemporaryUpload.
  *
- * @property int $id
- * @property string $session_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
- * @property-read int|null $media_count
+ * @property int                                                                                                                               $id
+ * @property string                                                                                                                            $session_id
+ * @property \Illuminate\Support\Carbon|null                                                                                                   $created_at
+ * @property \Illuminate\Support\Carbon|null                                                                                                   $updated_at
+ * @property \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property int|null                                                                                                                          $media_count
+ *
  * @method static Builder|TemporaryUpload newModelQuery()
  * @method static Builder|TemporaryUpload newQuery()
  * @method static Builder|TemporaryUpload query()
@@ -31,6 +32,7 @@ use Spatie\MediaLibrary\InteractsWithMedia;
  * @method static Builder|TemporaryUpload whereId($value)
  * @method static Builder|TemporaryUpload whereSessionId($value)
  * @method static Builder|TemporaryUpload whereUpdatedAt($value)
+ *
  * @mixin \Eloquent
  */
 class TemporaryUpload extends Model implements HasMedia
