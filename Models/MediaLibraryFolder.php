d11ea1f4e2245836c2bc1951b79b1c56a7d3c068
marco76tv <marco.sottana@gmail.com>
2023-11-03 12:13:18 +0100

    up

--- Models/MediaLibraryFolder.php
+++ Models/MediaLibraryFolder.php
@@ -17,47 +17,30 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 /**
  * Modules\Media\Models\MediaLibraryItem.
  *
- * @property int                                                                    $id
- * @property int|null                                                               $uploaded_by_user_id
- * @property string|null                                                            $caption
- * @property string|null                                                            $alt_text
- * @property int|null                                                               $folder_id
- * @property Carbon|null                                                            $created_at
- * @property Carbon|null                                                            $updated_at
- * @property int|null                                                               $order_column
- * @property \RalphJSmit\Filament\MediaLibrary\Media\Models\MediaLibraryFolder|null $folder
- * @property MediaCollection<int, Media>                                            $media
- * @property int|null                                                               $media_count
- * @property User|null                                                              $user
- *
- * @method static MediaLibraryFactory      factory($count = null, $state = [])
- * @method static Builder|MediaLibraryItem newModelQuery()
- * @method static Builder|MediaLibraryItem newQuery()
- * @method static Builder|MediaLibraryItem ordered(string $direction = 'asc')
- * @method static Builder|MediaLibraryItem query()
- * @method static Builder|MediaLibraryItem whereAltText($value)
- * @method static Builder|MediaLibraryItem whereCaption($value)
- * @method static Builder|MediaLibraryItem whereCreatedAt($value)
- * @method static Builder|MediaLibraryItem whereFolderId($value)
- * @method static Builder|MediaLibraryItem whereId($value)
- * @method static Builder|MediaLibraryItem whereOrderColumn($value)
- * @method static Builder|MediaLibraryItem whereUpdatedAt($value)
- * @method static Builder|MediaLibraryItem whereUploadedByUserId($value)
- *
- * @property int|null                                                          $parent_id
- * @property string                                                            $name
- * @property string|null                                                       $user_id
- * @property string|null                                                       $updated_by
- * @property string|null                                                       $created_by
- * @property \Illuminate\Database\Eloquent\Collection<int, MediaLibraryFolder> $children
- * @property int|null                                                          $children_count
- *
+ * @property int $id
+ * @property int|null $parent_id
+ * @property string $name
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $user_id
+ * @property string|null $updated_by
+ * @property string|null $created_by
+ * @property int|null $order_column
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, MediaLibraryFolder> $children
+ * @property-read int|null $children_count
+ * @method static Builder|MediaLibraryFolder newModelQuery()
+ * @method static Builder|MediaLibraryFolder newQuery()
+ * @method static Builder|MediaLibraryFolder ordered(string $direction = 'asc')
+ * @method static Builder|MediaLibraryFolder query()
+ * @method static Builder|MediaLibraryFolder whereCreatedAt($value)
  * @method static Builder|MediaLibraryFolder whereCreatedBy($value)
+ * @method static Builder|MediaLibraryFolder whereId($value)
  * @method static Builder|MediaLibraryFolder whereName($value)
+ * @method static Builder|MediaLibraryFolder whereOrderColumn($value)
  * @method static Builder|MediaLibraryFolder whereParentId($value)
+ * @method static Builder|MediaLibraryFolder whereUpdatedAt($value)
  * @method static Builder|MediaLibraryFolder whereUpdatedBy($value)
  * @method static Builder|MediaLibraryFolder whereUserId($value)
- *
  * @mixin \Eloquent
  */
 class MediaLibraryFolder extends BaseMediaLibraryFolder implements Sortable
