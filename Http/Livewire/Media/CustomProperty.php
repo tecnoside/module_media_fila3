6df44cebfd3dda765366c4688f07732b95fe0b2d
Malebestia <vair81@gmail.com>
2023-11-07 15:49:40 +0100

    .

--- Http/Livewire/Media/CustomProperty.php
+++ Http/Livewire/Media/CustomProperty.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 
 declare(strict_types=1);
@@ -64,66 +63,3 @@ class CustomProperty extends Component implements HasForms
         ];
     }
 }
-=======
-<?php
-
-declare(strict_types=1);
-
-namespace Modules\Media\Http\Livewire\Media;
-
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Contracts\HasForms;
-use Livewire\Component;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
-
-class CustomProperty extends Component implements HasForms
-{
-    use InteractsWithForms;
-
-    public string $attributeName;
-
-    public Media $media;
-
-    public function mount(Media $media, string $attributeName = 'description'): void
-    {
-        $this->media = $media;
-        $this->attributeName = $attributeName;
-        $value = $this->media->getCustomProperty($attributeName);
-        $this->form->fill([
-            $this->attributeName => $value,
-        ]);
-    }
-
-    public function render(): \Illuminate\Contracts\View\View
-    {
-        /**
-         * @phpstan-var view-string
-         */
-        // $view = app(GetViewAction::class)->execute();
-        $view = 'media::livewire.media.custom-property';
-
-        return view($view);
-    }
-
-    /**
-     * @return Textarea[]
-     *
-     * @psalm-return list{Textarea}
-     */
-    protected function getFormSchema(): array
-    {
-        return [
-            Textarea::make($this->attributeName)
-                ->label('')
-                // ->reactive()
-                ->lazy()
-                ->afterStateUpdated(function ($state): void {
-                    $this->media->setCustomProperty($this->attributeName, $state);
-                    $this->media->save();
-                }),
-            // ...
-        ];
-    }
-}
->>>>>>> Stashed changes
