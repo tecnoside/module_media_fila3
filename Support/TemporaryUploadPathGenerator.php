35af716ed107dfc42c50006c456f1d19a168c563
Malebestia <vair81@gmail.com>
2023-11-07 15:49:40 +0100

    .

--- Support/TemporaryUploadPathGenerator.php
+++ Support/TemporaryUploadPathGenerator.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 
 declare(strict_types=1);
@@ -44,47 +43,3 @@ class TemporaryUploadPathGenerator // implements PathGenerator
         return $key;
     }
 }
-=======
-<?php
-
-declare(strict_types=1);
-
-namespace Modules\Media\Support;
-
-use Modules\Media\Models\Media;
-use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
-
-class TemporaryUploadPathGenerator implements PathGenerator
-{
-    public function getPath(Media $media): string
-    {
-        return $this->getBasePath($media).'/'.md5($media->id.$media->uuid.'original').'/';
-    }
-
-    public function getPathForConversions(Media $media): string
-    {
-        return $this->getBasePath($media).'/'.md5($media->id.$media->uuid.'conversion');
-    }
-
-    public function getPathForResponsiveImages(Media $media): string
-    {
-        return $this->getBasePath($media).'/'.md5($media->id.$media->uuid.'responsive');
-    }
-
-    /*
-    * Get a unique base path for the given media.
-    */
-    protected function getBasePath(Media $media): string
-    {
-        $prefix = config('media-library.prefix', '');
-
-        $key = md5($media->uuid.$media->getKey());
-
-        if ('' !== $prefix) {
-            return $prefix.'/'.$key;
-        }
-
-        return $key;
-    }
-}
->>>>>>> Stashed changes
