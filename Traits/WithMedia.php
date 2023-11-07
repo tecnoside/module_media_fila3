04999c1e708c62913529e6f9410abb66ca10a818
marco76tv <marco.sottana@gmail.com>
2023-11-07 15:36:21 +0100

    up

--- Traits/WithMedia.php
+++ Traits/WithMedia.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 
 declare(strict_types=1);
@@ -80,3 +81,87 @@ trait WithMedia
             ->toArray();
     }
 }
+=======
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Media\Traits;
+
+use Livewire\Component;
+
+/** @mixin Component */
+trait WithMedia
+{
+    /**
+     * @return string[]
+     *
+     * @psalm-return array<string>
+     */
+    public function getMediaComponentNames(): array
+    {
+        return $this->mediaComponentNames ?? [];
+    }
+
+    public function mountWithMedia(): void
+    {
+        foreach ($this->getMediaComponentNames() as $mediaComponentName) {
+            $this->$mediaComponentName = null;
+        }
+    }
+
+    public function hydrateWithMedia(): void
+    {
+        foreach ($this->getMediaComponentNames() as $mediaComponentName) {
+            $this->listeners["{$mediaComponentName}:mediaChanged"] = 'onMediaChanged';
+        }
+    }
+
+    public function onMediaChanged(string $name, array $media): void
+    {
+        $media = $this->makeSureCustomPropertiesUseRightCasing($media);
+
+        $this->$name = $media;
+    }
+
+    public function renderingWithMedia(): void
+    {
+        $errorBag = $this->getErrorBag();
+
+        foreach ($this->getMediaComponentNames() as $mediaComponentName) {
+            $this->emit("{$mediaComponentName}:mediaComponentValidationErrors", $mediaComponentName, $errorBag->toArray());
+        }
+    }
+
+    public function clearMedia($mediaComponentNames = null): void
+    {
+        if (null === $mediaComponentNames) {
+            $mediaComponentNames = $this->getMediaComponentNames();
+        }
+
+        if (\is_string($mediaComponentNames)) {
+            $mediaComponentNames = [$mediaComponentNames];
+        }
+
+        foreach ($mediaComponentNames as $mediumComponentName) {
+            $this->emit("{$mediumComponentName}:clearMedia", $mediumComponentName);
+
+            $this->{$mediumComponentName} = [];
+        }
+    }
+
+    protected function makeSureCustomPropertiesUseRightCasing(array $media): array
+    {
+        return collect($media)
+            ->map(function (array $mediaItemAttributes): array {
+                if (! isset($mediaItemAttributes['custom_properties']) && isset($mediaItemAttributes['customProperties'])) {
+                    $mediaItemAttributes['custom_properties'] = $mediaItemAttributes['customProperties'];
+                    unset($mediaItemAttributes['customProperties']);
+                }
+
+                return $mediaItemAttributes;
+            })
+            ->toArray();
+    }
+}
+>>>>>>> Stashed changes
--- View/Components/VideoPlayer.php
+++ View/Components/VideoPlayer.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 
 declare(strict_types=1);
@@ -48,3 +49,55 @@ class VideoPlayer extends Component
         return view($view, $view_params);
     }
 }
+=======
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Media\View\Components;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Modules\Xot\Actions\GetViewAction;
+
+/**
+ * Class VideoPlayer.
+ */
+class VideoPlayer extends Component
+{
+    public string $driver;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(public string $mp4Src, public int $currentTime, string $driver = null)
+    {
+        if (null === $driver) {
+            $driver = config('xra.video.player');
+        }
+        $this->driver = $driver;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return View
+     */
+    public function render()
+    {
+        /**
+         * @phpstan-var view-string
+         */
+        // $view = 'media::components.video-player.'.$this->driver;
+        $view = app(GetViewAction::class)->execute($this->driver);
+
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view($view, $view_params);
+    }
+}
+>>>>>>> Stashed changes
