340e0d0432806e5248a18ac801dd10857c8da1ea
marco76tv <marco.sottana@gmail.com>
2023-11-14 12:31:32 +0100

    up

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,7 +7,7 @@ require('laravel-mix-merge-manifest');
 mix.setPublicPath('../../public').mergeManifest();
 
 mix.js(__dirname + '/Resources/assets/js/app.js', 'js/media.js')
-    .sass( __dirname + '/Resources/assets/sass/app.scss', 'css/media.css');
+    .sass(__dirname + '/Resources/assets/sass/app.scss', 'css/media.css');
 
 if (mix.inProduction()) {
     mix.version();
