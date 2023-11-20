bc6d9897a3adbbaaa2d1b8c3e3848504d0ec2c3f
marco76tv <marco.sottana@gmail.com>
2023-11-20 09:25:04 +0100

    up

--- docs/config.php
+++ docs/config.php
@@ -18,19 +18,6 @@ return [
     'navigation' => require_once('navigation.php'),
 
     // helpers
-<<<<<<< HEAD
-    'isActive' => static function ($page, $path) {
-        return Str::endsWith(trimPath($page->getPath()), trimPath($path));
-    },
-    'isActiveParent' => static function ($page, $menuItem) {
-        if (is_object($menuItem) && $menuItem->children) {
-            return $menuItem->children->contains(static function ($child) use ($page) {
-                return trimPath($page->getPath()) === trimPath($child);
-            });
-        }
-    },
-    'url' => static function ($page, $path) {
-=======
     'isActive' => function ($page, $path) {
         return Str::endsWith(trimPath($page->getPath()), trimPath($path));
     },
@@ -42,7 +29,6 @@ return [
         }
     },
     'url' => function ($page, $path) {
->>>>>>> f67977ef77e73e042322e1befe1802612e193f15
         return Str::startsWith($path, 'http') ? $path : '/'.trimPath($path);
     },
 ];
