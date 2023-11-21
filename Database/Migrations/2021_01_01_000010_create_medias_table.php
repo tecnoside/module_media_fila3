ef983a13b850240fa30900a9ea47bbbe3835ef46
marco76tv <marco.sottana@gmail.com>
2023-11-21 15:53:48 +0100

    up

--- Database/Migrations/2021_01_01_000010_create_medias_table.php
+++ Database/Migrations/2021_01_01_000010_create_medias_table.php
@@ -23,7 +23,7 @@ class CreateMediasTable extends XotBaseMigration
         // -- CREATE --
         $this->tableCreate(function (Blueprint $table) : void {
             $table->bigIncrements('id');
-            // $table->morphs('model');
+            //$table->morphs('model');
             $table->uuidMorphs('model');
             $table->uuid('uuid')->nullable()->unique();
             $table->string('collection_name');
@@ -112,7 +112,6 @@ class CreateMediasTable extends XotBaseMigration
             if (! $this->hasColumn('curations')) {
                 $table->longText('curations')->nullable();
             }
-
             $this->updateUser($table);
         });
     }
