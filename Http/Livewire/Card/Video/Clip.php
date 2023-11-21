48a956089427c0502851be350ea4a43c454a8c44
marco76tv <marco76tv@users.noreply.github.com>
2023-11-21 09:39:20 +0000

    Check & fix styling

--- Http/Livewire/Card/Video/Clip.php
+++ Http/Livewire/Card/Video/Clip.php
@@ -67,7 +67,7 @@ class Clip extends Component
      */
     public function updateDataFromModal(string $id, array $data): void
     {
-        if ($id !== 'editClip') {
+        if ('editClip' !== $id) {
             return;
         }
         if ($data['id'] !== $this->model->getKey()) {
