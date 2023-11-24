aa93d3336ed4b56d5e3c996f51317a79001b3019
marco76tv <marco.sottana@gmail.com>
2023-11-24 16:31:12 +0100

    Add media properties to MediaLibraryItem and FilamentMediaLibraryModel

--- Models/MediaLibraryFolder.php
+++ Models/MediaLibraryFolder.php
@@ -14,12 +14,30 @@ use Spatie\EloquentSortable\SortableTrait;
 /**
  * Modules\Media\Models\MediaLibraryItem.
  *
+ * @property int $id
+ * @property int|null $parent_id
+ * @property string $name
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $user_id
+ * @property string|null $updated_by
+ * @property string|null $created_by
+ * @property int|null $order_column
  * @property-read Collection<int, MediaLibraryFolder> $children
  * @property-read int|null $children_count
  * @method static Builder|MediaLibraryFolder newModelQuery()
  * @method static Builder|MediaLibraryFolder newQuery()
  * @method static Builder|MediaLibraryFolder ordered(string $direction = 'asc')
  * @method static Builder|MediaLibraryFolder query()
+ * @method static Builder|MediaLibraryFolder whereCreatedAt($value)
+ * @method static Builder|MediaLibraryFolder whereCreatedBy($value)
+ * @method static Builder|MediaLibraryFolder whereId($value)
+ * @method static Builder|MediaLibraryFolder whereName($value)
+ * @method static Builder|MediaLibraryFolder whereOrderColumn($value)
+ * @method static Builder|MediaLibraryFolder whereParentId($value)
+ * @method static Builder|MediaLibraryFolder whereUpdatedAt($value)
+ * @method static Builder|MediaLibraryFolder whereUpdatedBy($value)
+ * @method static Builder|MediaLibraryFolder whereUserId($value)
  * @mixin \Eloquent
  */
 class MediaLibraryFolder extends BaseMediaLibraryFolder implements Sortable
