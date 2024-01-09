de2159d9f38e531265de08a9a0774cfb4ed32fe6
Malebestia <vair81@gmail.com>
2024-01-09 16:57:24 +0100

    .

--- Routes/web.php
+++ Routes/web.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/stream/{media}', 'StreamController');
 // Route::get('/streamsnip/{media}', 'StreamsnipController');
 
+
 Route::get('/media/videoimg/{sec}', 'MediaVideoImgController@getSecond')->name('media.videoimg.get_second');
