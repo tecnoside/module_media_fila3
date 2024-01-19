f00c7eca956f3a215ca6ea46b12f4ab465700429
marco76tv <marco.sottana@gmail.com>
2024-01-19 11:44:11 +0100

    up

--- Config/config.php
+++ Config/config.php
@@ -2,19 +2,6 @@
 
 return [
     'name' => 'Media',
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     'icon' => 'fas-photo-video',
-=======
     'navigation_sort' => 1,
->>>>>>> 771f698d (first)
-=======
->>>>>>> 7cc85766 (rebase 1)
-=======
->>>>>>> 76f3bf5f (first)
-=======
-    'icon' => 'fas-photo-video',
->>>>>>> b9ba8917 (.)
 ];
