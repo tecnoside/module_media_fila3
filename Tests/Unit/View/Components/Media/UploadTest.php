d1d821abf0ae31d4e5f4f4abe6a6a6e0cac8198a
marco76tv <marco.sottana@gmail.com>
2024-01-19 11:03:18 +0100

    up

---
+++ Tests/Unit/View/Components/Media/UploadTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Modules\Media\View\Tests\Unit\Components\Media;
+
+use Modules\Media\View\Components\Media\Upload;
+use Tests\TestCase;
+
+/**
+ * Class UploadTest.
+ *
+ * @covers \Modules\Media\View\Components\Media\Upload
+ */
+final class UploadTest extends TestCase
+{
+    private Upload $upload;
+
+    private string $name;
+
+    private string $rules;
+
+    private bool $multiple;
+
+    private bool $editableName;
+
+    private int $maxItems;
+
+    private string $componentView;
+
+    private string $listView;
+
+    private string $itemView;
+
+    private string $propertiesView;
+
+    private string $fieldsView;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->name = '42';
+        $this->rules = '42';
+        $this->multiple = true;
+        $this->editableName = true;
+        $this->maxItems = 42;
+        $this->componentView = '42';
+        $this->listView = '42';
+        $this->itemView = '42';
+        $this->propertiesView = '42';
+        $this->fieldsView = '42';
+        $this->upload = new Upload($this->name, $this->rules, $this->multiple, $this->editableName, $this->maxItems, $this->componentView, $this->listView, $this->itemView, $this->propertiesView, $this->fieldsView);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        unset($this->upload);
+        unset($this->name);
+        unset($this->rules);
+        unset($this->multiple);
+        unset($this->editableName);
+        unset($this->maxItems);
+        unset($this->componentView);
+        unset($this->listView);
+        unset($this->itemView);
+        unset($this->propertiesView);
+        unset($this->fieldsView);
+    }
+
+    public function testRender(): void
+    {
+        /** @todo This test is incomplete. */
+        self::markTestIncomplete();
+    }
+
+    public function testDetermineListViewName(): void
+    {
+        /** @todo This test is incomplete. */
+        self::markTestIncomplete();
+    }
+
+    public function testDetermineItemViewName(): void
+    {
+        /** @todo This test is incomplete. */
+        self::markTestIncomplete();
+    }
+
+    public function testDetermineFieldsViewName(): void
+    {
+        /** @todo This test is incomplete. */
+        self::markTestIncomplete();
+    }
+
+    public function testDetermineMaxItems(): void
+    {
+        /** @todo This test is incomplete. */
+        self::markTestIncomplete();
+    }
+}
