963aa2680efdc6f132d4b1393bff90413baaeced
marco76tv <marco.sottana@gmail.com>
2024-01-22 17:01:58 +0100

    🔧 refactor(MediaDatabaseSeeder): remove unused code and comments 🔧 refactor(MediaDatabaseSeederTest): remove unused code and comments, correct namespace declaration 🔧 refactor(ClipTest): remove unused code and comments, correct namespace declaration 🔧 refactor(UploadTest): remove unused code and comments, correct namespace declaration

--- Tests/Unit/Http/Livewire/Card/Video/ClipTest.php
+++ Tests/Unit/Http/Livewire/Card/Video/ClipTest.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Modules\Media\Http\Livewire\Tests\Unit\Card\Video;
+declare(strict_types=1);
+
+namespace Modules\Media\Tests\Unit\Http\Livewire\Card\Video;
 
 use Modules\Media\Http\Livewire\Card\Video\Clip;
 use Tests\TestCase;
@@ -14,20 +16,14 @@ final class ClipTest extends TestCase
 {
     private Clip $clip;
 
-    /**
-     * {@inheritdoc}
-     */
     protected function setUp(): void
     {
         parent::setUp();
 
-        /** @todo Correctly instantiate tested object to use it. */
+        /* @todo Correctly instantiate tested object to use it. */
         $this->clip = new Clip();
     }
 
-    /**
-     * {@inheritdoc}
-     */
     protected function tearDown(): void
     {
         parent::tearDown();
@@ -37,25 +33,25 @@ final class ClipTest extends TestCase
 
     public function testMount(): void
     {
-        /** @todo This test is incomplete. */
+        /* @todo This test is incomplete. */
         self::markTestIncomplete();
     }
 
     public function testRender(): void
     {
-        /** @todo This test is incomplete. */
+        /* @todo This test is incomplete. */
         self::markTestIncomplete();
     }
 
     public function testEditClip(): void
     {
-        /** @todo This test is incomplete. */
+        /* @todo This test is incomplete. */
         self::markTestIncomplete();
     }
 
     public function testUpdateDataFromModal(): void
     {
-        /** @todo This test is incomplete. */
+        /* @todo This test is incomplete. */
         self::markTestIncomplete();
     }
 }
