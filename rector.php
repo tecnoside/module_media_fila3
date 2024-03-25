d3383620d755469491b8293dc7f064cb8d7e59ec
marco76tv <marco.sottana@gmail.com>
2024-03-25 12:04:59 +0100

    up

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'',
+    ])
+    ->skip([
+        __DIR__.'/vendor',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ]);
