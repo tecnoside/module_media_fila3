e1a92171f05f109e60d3eb7e483cf5d330c8803c
marco <marco.sottana@gmail.com>
2024-06-14 10:49:05 +0200

    .

--- Providers/Filament/AdminPanelProvider.php
+++ Providers/Filament/AdminPanelProvider.php
@@ -11,24 +11,10 @@ class AdminPanelProvider extends XotBasePanelProvider
 {
     protected string $module = 'Media';
 
-    // public function panel(Panel $panel): Panel
-    // {
-    //     $panel = parent::panel($panel);
+    public function panel(Panel $panel): Panel
+    {
+        $panel = parent::panel($panel);
 
-    //     return $panel->plugins([
-    //         \Awcodes\Curator\CuratorPlugin::make()
-    //             ->label('Curator Media')
-    //             ->pluralLabel('Curator Media')
-    //             ->navigationIcon('heroicon-o-photo')
-    //             ->navigationGroup('Content')
-    //             ->navigationSort(3)
-    //             ->navigationCountBadge()
-    //             ->registerNavigation(false)
-    //         // ->defaultListView('grid' || 'list'),
-    //             ->defaultListView('grid')
-    //         // ->resource(\App\Filament\Resources\CustomMediaResource::class),
-    //         // ->resource(\Modules\Media\Filament\Resources\CuratorMediaResource::class)
-    //         ,
-    //     ]);
-    // }
+        return $panel;
+    }
 }
