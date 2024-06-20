05f0cfbd84fe077da2909e0185086f2192a863cc
marco <marco.sottana@gmail.com>
2024-06-20 11:41:41 +0200

    .

--- Resources/views/filament/pages/dashboard.blade.php
+++ Resources/views/filament/pages/dashboard.blade.php
@@ -1,5 +1,11 @@
 <x-filament::page>
     {{--
+<<<<<<< HEAD
    
+=======
+    <img src="{{ ui::asset('ewall::img/logo.png') }}" />
+
+    <img src="{{ $ui->asset('ewall::img/logo.png') }}" />
+>>>>>>> e12fd656 (up)
     --}}
 </x-filament::page>
