5a4dc4b77e92243563c50adeeee0047d92a38cdf
marco <marco.sottana@gmail.com>
2024-06-21 12:35:59 +0200

    up

--- Filament/Resources/MediaResource/Pages/ViewMedia.php
+++ Filament/Resources/MediaResource/Pages/ViewMedia.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Media\Filament\Resources\MediaResource\Pages;
 
 use Filament\Actions\DeleteAction;
+use Filament\Forms\Components\Radio;
 use Filament\Infolists\Components\Actions;
 use Filament\Infolists\Components\Actions\Action;
 use Filament\Infolists\Components\ImageEntry;
@@ -13,6 +14,7 @@ use Filament\Infolists\Components\Split;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
+use Modules\Media\Filament\Infolists\VideoEntry;
 use Modules\Media\Filament\Resources\MediaResource;
 
 class ViewMedia extends ViewRecord
@@ -42,27 +44,39 @@ class ViewMedia extends ViewRecord
                     [
                         Section::make()->schema(
                             [
-                                ImageEntry::make('image')
+                                ImageEntry::make('src')
                                     ->label('')
                                     ->defaultImageUrl(fn ($record) => $record->getUrl())
-                                    ->size(500),
+                                    ->size(500)
+                                    ->visible(fn ($record) => 'image' == $record->type),
+
+                                VideoEntry::make('src')
+                                    ->label('')
+                                    ->defaultImageUrl(fn ($record) => $record->getUrl())
+                                    ->size(500)
+                                    ->visible(fn ($record) => 'video' == $record->type),
                             ]
                         ),
                         Section::make()->schema(
                             [
                                 Actions::make([
-                                    Action::make('star')
-                                        ->icon('heroicon-m-star')
-                                        ->requiresConfirmation()
-                                        ->action(function (Star $star) {
-                                            $star();
-                                        }),
-                                    Action::make('resetStars')
-                                        ->icon('heroicon-m-x-mark')
-                                        ->color('danger')
-                                        ->requiresConfirmation()
-                                        ->action(function (ResetStars $resetStars) {
-                                            $resetStars();
+                                    Action::make('convert')
+                                        ->label('')
+                                        ->tooltip('convert')
+                                        ->icon('heroicon-o-scale')
+                                        // ->requiresConfirmation()
+                                        ->form([
+                                            Radio::make('format')
+                                                ->label('Format?')
+                                                ->options([
+                                                    'webm01' => 'webm01',
+                                                    'webm02' => 'webm02',
+                                                ])
+                                                ->inline()
+                                                ->inlineLabel(false),
+                                        ])
+                                        ->action(function ($record, $data) {
+                                            dddx([$record, $data]);
                                         }),
                                 ]),
                                 TextEntry::make('name'),
