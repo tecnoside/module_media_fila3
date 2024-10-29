640820f2b8f8de318912bee9c53f02e82ee3002b
marco <marco.sottana@gmail.com>
2024-10-29 17:34:28 +0100

    up

--- Models/Media.php
+++ Models/Media.php
@@ -327,7 +327,7 @@ class Media extends SpatieMedia
     }
 
     /** @return array<string, string> */
-    public function casts(): array
+    protected function casts(): array
     {
         return [
             'id' => 'string',
